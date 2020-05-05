7cd29ec066da95992f3cc78dd4a4c3abe28a7e77
Anna Romashenko <aa.romashchenko@gmail.com>
2020-05-05 19:05:22 +0300

    Updates

---
+++ precache-manifest.9a26e224f7df2cc7f4bde955fb3366a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b74043a46e90d6a67a2ee5a042675663",
+    "url": "/react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "d91a209fa274b7633f8f",
+    "url": "/react-hw-02-feedback/static/css/main.1736a220.chunk.css"
+  },
+  {
+    "revision": "9a3cbe7fc50852fb2a2c",
+    "url": "/react-hw-02-feedback/static/js/2.9b5760dd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-hw-02-feedback/static/js/2.9b5760dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d91a209fa274b7633f8f",
+    "url": "/react-hw-02-feedback/static/js/main.3344a382.chunk.js"
+  },
+  {
+    "revision": "9fb8e3fe06d6cd3b88cc",
+    "url": "/react-hw-02-feedback/static/js/runtime-main.1effbeed.js"
+  }
+]);
